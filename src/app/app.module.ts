b6e7ebc3e1cc46be3b1bca4653aa67b4fa6ec522
daotqph08312 <daotqph08312@fpt.edu.vn>
2020-03-31 20:06:16 +0700

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SchollService } from './scholl.service';
+import { ClassService } from './class.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [SchollService, ClassService]
 })
 export class AppModule { }
---
+++ src/app/scholl.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+const schoolAPI = "https://5e7c6ee4a917d70016683411.mockapi.io/schools";
+@Injectable()
+export class SchollService {
+
+  constructor( private http: HttpClient) { }
+  getListSchools(): Observable<any>{
+      return this.http.get<any>(schoolAPI);
+  };
+}
