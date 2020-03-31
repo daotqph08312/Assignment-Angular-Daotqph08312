791ed1211f0003df177070b45d21b9dc28b587ad
daotqph08312 <daotqph08312@fpt.edu.vn>
2020-03-31 20:20:36 +0700

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {RouterModule} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SchollService } from './scholl.service';
 import { ClassService } from './class.service';
+import { ClassComponentComponent } from './class-component/class-component.component';
+import { SchoolComponentComponent } from './school-component/school-component.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule ,
+   RouterModule.forRoot([
+     {path:'', component:SchoolComponentComponent},
+     {path: 'class/id', component:ClassComponentComponent}
+      
+    ])
+  ],
+  
+  declarations: [ AppComponent, HelloComponent,  ClassComponentComponent, SchoolComponentComponent ],
   bootstrap:    [ AppComponent ],
   providers: [SchollService, ClassService]
 })
---
+++ src/app/school-component/school-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-school-component',
+  templateUrl: './school-component.component.html',
+  styleUrls: ['./school-component.component.css']
+})
+export class SchoolComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
