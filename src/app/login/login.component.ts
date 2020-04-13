335ab230d6d9cc0736f162329d1b2c4e310c3f26
daotqph08312 <daotqph08312@fpt.edu.vn>
2020-04-13 16:17:47 +0700

    2 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,7 +12,7 @@ import {User} from '../user';
 export class LoginComponent implements OnInit {
       loginForm: FormGroup;
     submitted = false;
-    message: String;
+    message:String = '';
  
 
   constructor(
