5efe3a56c94acd31a45b309c93c1fae458e9b4ca
daotqph08312 <daotqph08312@fpt.edu.vn>
2020-04-13 13:19:04 +0700

    2 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router,ActivatedRoute} from "@angular/router"
 import { FormControl,FormGroup } from "@angular/forms";
+import { FormBuilder, Validators } from '@angular/forms';
 import {UserService} from '../user.service';
 import {User} from '../user';
 @Component({
@@ -9,29 +10,46 @@ import {User} from '../user';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-      message = null;
-     username= new FormControl('')
-     password= new FormControl('');
+      loginForm: FormGroup;
+      submited: false;
+ 
+ 
 
   constructor(
     private route: Router,
      private activateRoute:ActivatedRoute,
-     private UserService:UserService
+     private UserService:UserService,
+     private formBuider:FormBuilder
   ) {  }
 
   ngOnInit() {
+    this.loginForm = this.formBuider.group({
+        username: ['', Validators.required],
+        password:['',Validators.required],
+        acceptTerms: [false,Validators.requiredTrue]
+    });
+
   }
+  regiserToField(){ return this.loginForm.controls}
   login(){
-      this.UserService.login(this.username.value).subscribe(data =>{
-            if(this.username.value == data.username && this.password.value == data.password){
-              console.log('success');
-              this.route.navigate(['home/index']);
-            }else{
-              this.message = 'Tài Khoản Hoặc Mật Khẩu không đúng';
-               this.route.navigate(['']);
+     
+     if(!this.loginForm.invalid){
+       console.log(this.loginForm.value.username);
+       return true;
+     }
+//      else{
+//  this.UserService.login(this.loginForm.value).subscribe(data =>{
+//             if(this.loginForm.value.username == data.username && this.loginForm.value.password == data.password){
+//               console.log('success');
+//               this.route.navigate(['home/index']);
+//             }else{
+//               // this.message = 'Tài Khoản Hoặc Mật Khẩu không đúng';
+//                this.route.navigate(['']);
                
-            }
-      });
+//             }
+//       });
+//      }
+     
       
   }
 }
